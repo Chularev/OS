67e97ecfbbff21c1c8515952693866e5ed2dd168
Alex <chulgarev@gmail.com>
2025-06-19 18:22:32 +0700

    done tmp

--- src/main.asm
+++ src/main.asm
@@ -2,6 +2,9 @@ org 0x7C00
 bits 16
 
 
+%define ENDL 0x0D, 0x0A
+
+
 _start:
     ; init
     mov ax, 0           ; can't set ds/es directly
@@ -14,42 +17,34 @@ _start:
 
     cli
 
-    ; Install INT 1Ch handler
-    mov word [0x1C*4], timer_isr    ; Offset
-    mov [0x1C*4+2], cs              ; Segment (current code segment)
+    mov word [ds:0x1C*4], timer_isr    ; Set handler OFFSET
+    mov [ds:0x1C*4+2], cs              ; Set handler SEGMENT (CS)
     sti                   ; Re-enable interrupts
-
-    ; Main loop - wait for 18 ticks (~1 second)
-    mov cx, 18
-.wait_loop:
-    cmp [tick_count], cx
-    jb .wait_loop
-
-    ; Print message after delay
-    mov si, msg
-    call print
-    hlt
 
 ; INT 1Ch handler (called ~18.2 times/sec)
 timer_isr:
-    call print
-    inc word [tick_count]
-    iret                  ; No need to chain for INT 1Ch
+    push ax
+    push si
+    push bx
 
-; Simple print function
-print:
-    lodsb
-    test al, al
-    jz .done
-    mov ah, 0x0E
-    int 0x10
-    jmp print
-.done:
-    ret
+    mov si, msg
 
-; Data
-tick_count dw 0
-msg db "1 second passed!", 0
+    .loop:
+        lodsb
+        test al, al
+        jz .done
+        mov ah, 0x0E
+        mov bh, 0 
+        int 0x10
+        jmp .loop
+
+    .done:
+        pop ax
+        pop si
+        pop bx
+        ret
+
+msg db "1 second passed!", ENDL, 0
 
 times 510-($-$$) db 0
 dw 0AA55h
